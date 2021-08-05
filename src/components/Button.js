64b50cae1f97944c67109ce73b245ebc515f9125
SeungHyun <fm10033@gmail.com>
2021-08-05 17:36:46 +0900

    size props 설정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Button.scss";
+import classnames from "classnames";
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
+function Button({ children, size }) {
+  return <button className={classnames("button", size)}>{children}</button>;
 }
 
+Button.defaultProps = {
+  size: "medium",
+};
 export default Button;
